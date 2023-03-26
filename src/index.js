10bcbc0fabc0261955a922527811b74ff8b1fe78
acwik166 <maciekposala11@gmail.com>
2023-03-26 13:56:31 +0200

    eqweqw

--- src/index.js
+++ src/index.js
@@ -3,86 +3,101 @@ dotenv.config()
 
 import express from 'express'
 import mongoose from 'mongoose'
-import bodyParser from 'body-parser'
-import passport from 'passport'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
 
-import User from './models/User.js'
-import Article from './models/Article.js'
+const app = express();
+const port = process.env.PORT || 3333;
 
-import auth from './routes/auth.js'
-import articles from './routes/articles.js'
-import comments from './routes/comments.js'
+app.use(express.json());
+app.use(express.raw({ type: "application/vnd.custom-type" }));
+app.use(express.text({ type: "text/html" }));
 
-passport.use(User.createStrategy())
+app.get("/", async (req, res) => {
+    res.json({ message: "Please visit /countries to view all the countries" });
+});
 
-mongoose.connect(process.env.MONGO_URI)
 
-const app = express()
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser())
+// import bodyParser from 'body-parser'
+// import passport from 'passport'
+// import cookieParser from 'cookie-parser'
+// import cors from 'cors'
+
+// import User from './models/User.js'
+// import Article from './models/Article.js'
+
+// import auth from './routes/auth.js'
+// import articles from './routes/articles.js'
+// import comments from './routes/comments.js'
+
+// passport.use(User.createStrategy())
+
+// mongoose.connect(process.env.MONGO_URI)
+
+// const app = express()
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(cookieParser())
 // app.use(cors({
 //     origin: '*',
 //     credentials: true,
 // }))
 // app.use(cors())
 
-app.use('/api/articles', articles)
-app.use('/api/auth', auth)
-app.use('/api/comments/', comments)
+// app.use('/api/articles', articles)
+// app.use('/api/auth', auth)
+// app.use('/api/comments/', comments)
 
 
 // Socket
-import { Server } from 'socket.io'
-const io = new Server(process.env.PORT_SOCKET)
-
-// , {
+// import { Server } from 'socket.io'
+// const io = new Server(process.env.PORT_SOCKET, {
 //     cors: {
 //         origin: '',
 //         methods: ['GET', 'POST'],
 //     }
+// })
+
+// io.on('connection', socket => {
+//     socket.on('get-document', async ({ documentId, userId }) => {
+//         const document = await findOrCreateArticle(documentId, userId)
+//         socket.join(documentId)
+//         socket.emit('load-document', document)
+
+//         socket.on('send-changes', (delta) => {
+//             socket.broadcast.to(documentId).emit('receive-changes', delta)
+//         })
+
+//         socket.on('send-title', (title) => {
+//             socket.broadcast.to(documentId).emit('receive-title', title)
+//         })
+
+//         socket.on('save-document', async (data) => {
+//             await Article.findByIdAndUpdate(documentId, { data: data })
+//         })
+
+//         socket.on('save-title', async (title) => {
+//             await Article.findByIdAndUpdate(documentId, { title: title })
+//         })
+//     })
+// })
+
+
+// async function findOrCreateArticle(docId, userId) {
+//     if (docId == null && userId == null) return
+
+//     const document = await Article.findById(docId)
+
+//     if (document) return document
+
+//     console.log(userId)
+
+//     return await Article.create({ _id: docId, author: userId, data: '', title: '' })
 // }
 
-io.on('connection', socket => {
-    socket.on('get-document', async ({ documentId, userId }) => {
-        const document = await findOrCreateArticle(documentId, userId)
-        socket.join(documentId)
-        socket.emit('load-document', document)
-
-        socket.on('send-changes', (delta) => {
-            socket.broadcast.to(documentId).emit('receive-changes', delta)
-        })
-
-        socket.on('send-title', (title) => {
-            socket.broadcast.to(documentId).emit('receive-title', title)
-        })
-
-        socket.on('save-document', async (data) => {
-            await Article.findByIdAndUpdate(documentId, { data: data })
-        })
-
-        socket.on('save-title', async (title) => {
-            await Article.findByIdAndUpdate(documentId, { title: title })
-        })
-    })
-})
-
-
-async function findOrCreateArticle(docId, userId) {
-    if (docId == null && userId == null) return
-
-    const document = await Article.findById(docId)
-
-    if (document) return document
-
-    console.log(userId)
-
-    return await Article.create({ _id: docId, author: userId, data: '', title: '' })
-}
-
-app.listen(process.env.PORT_API, () => {
-    console.log('server running..')
-})
+// app.listen(process.env.PORT_API, () => {
+//     console.log('server running..')
+// })
